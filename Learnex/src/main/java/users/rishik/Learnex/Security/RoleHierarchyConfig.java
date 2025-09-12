94c22fd627399b3d24d29540fe773834f2d196d9
Risspecct <rishikgarg.dev@gmail.com>
2025-09-13 02:39:25 +0530

    Added security related classes

---
+++ Learnex/src/main/java/users/rishik/Learnex/Security/RoleHierarchyConfig.java
@@ -0,0 +1,13 @@
+package users.rishik.Learnex.Security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+
+@Configuration
+public class RoleHierarchyConfig {
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy("ROLE_TEACHER >  ROLE_STUDENT");
+    }
+}
---
+++ Learnex/src/test/java/users/rishik/Learnex/LearnexApplicationTests.java
@@ -0,0 +1,13 @@
+package users.rishik.Learnex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
