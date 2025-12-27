399bf485ea002d4d77d3d607d541c4f1e5365276
Risspecct <rishikgarg.dev@gmail.com>
2025-12-27 23:52:48 +0530

    Added docker file for Python

--- backend/Python/main.py
+++ backend/Python/main.py
@@ -3,19 +3,19 @@ from features.translator_api import router as translator_router
 from features.ai_router import router as ai_router
 from fastapi.middleware.cors import CORSMiddleware
 from Syllabus.syllabus_router import router as syllabus_router
-import uvicorn
+import os
+# import uvicorn
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:8080",
-]
+origins = os.getenv("CORS_ORIGINS", "").split(",")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"], # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"], # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(translator_router, prefix="/api",tags=["translator"])
@@ -26,5 +26,5 @@ app.include_router(syllabus_router, prefix="/syllabus", tags=["Syllabus"])
 def home():
     return {"message": "Welcome to the Learnex Backend!"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
